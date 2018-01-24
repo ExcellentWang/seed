81101bc2f2c0f6ac3f6284aa5e8555a4e728c60e
wang <excellentwanglei@hotmail.com>
2018-01-24 11:44:27 +0800

    refresh part of table and complete verfiycodesend and regist

--- src/main/java/com/company/restapi/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/company/restapi/service/impl/AppUserServiceImpl.java
@@ -6,6 +6,9 @@ import com.company.restapi.dao.IntelAccountMapper;
 import com.company.restapi.model.IntelAccount;
 import com.company.restapi.service.AppUserService;
 import com.company.restapi.service.SmsService;
+import com.company.restapi.web.AppUserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +30,8 @@ import java.util.concurrent.TimeUnit;
 @Service
 @Transactional
 public class AppUserServiceImpl implements AppUserService {
+    private final static Logger logger = LoggerFactory.getLogger(AppUserServiceImpl.class);
+
     //短信服务
     @Resource
     private SmsService smsService;
@@ -36,43 +41,143 @@ public class AppUserServiceImpl implements AppUserService {
     //redis服务
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Map<Object, Object> sendVerfiyCode(String phone) {
         String verification = StringUtilsCommon.getRandNum(6);
-        Map<Object, Object> objectObjectMap = smsService.sendVerificationCode(phone,verification);
+        Map<Object, Object> objectObjectMap = smsService.sendVerificationCode(phone, verification);
         //验证码存入redis
-        stringRedisTemplate.opsForValue().set(phone,verification,10, TimeUnit.SECONDS);
+        stringRedisTemplate.opsForValue().set(phone, verification, 10, TimeUnit.SECONDS);
+        logger.info(verification);
         return objectObjectMap;
     }
 
     @Override
-    public Map<Object, Object> addNewUser(IntelAccount intelAccount, String code) {
+    public Map<Object, Object> userRegist(IntelAccount intelAccount, String code) {
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
         //redis取数据
         String verifyCode = stringRedisTemplate.opsForValue().get(intelAccount.getPhoneNo());
         //验证码校验
-        if(verifyCode !=code){
+        if (!verifyCode.equals(code) ) {
             objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
             objectObjectHashMap.put("msg", "用户的验证码错误...");
             return objectObjectHashMap;
         }
         //查询当前注册用户
         IntelAccount intelAccount1 = intelAccountMapper.selectOne(intelAccount);
-        if (intelAccount1.getPhoneNo()==intelAccount.getPhoneNo()) {
+        if (intelAccount1.getPhoneNo().equals(intelAccount.getPhoneNo()) ) {
             objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
             objectObjectHashMap.put("msg", "该用户名已经注册过...");
-            return  objectObjectHashMap;
+            return objectObjectHashMap;
         }
         //插入当前用户
         int insert = intelAccountMapper.insert(intelAccount);
         if (insert == 0) {
             objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
             objectObjectHashMap.put("msg", "新建用户失败...");
-            return  objectObjectHashMap;
+            return objectObjectHashMap;
         }
 
         objectObjectHashMap.put("code", BaseConstant.appUserSuccessStatus);
         objectObjectHashMap.put("msg", "用户注册成功...");
+        return objectObjectHashMap;
+    }
+
+    @Override
+    public Map<Object, Object> userLogin(IntelAccount intelAccount) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        //查询当前登陆用户
+        IntelAccount intelAccount1 = intelAccountMapper.selectOne(intelAccount);
+        if (intelAccount1.getId() == 0) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "当前用户未注册...");
+            return objectObjectHashMap;
+        }
+        //生成token存redis
+        String token = StringUtilsCommon.getToken();
+        stringRedisTemplate.opsForValue().set(intelAccount1.getId().toString(), token, 30, TimeUnit.MINUTES);
+        objectObjectHashMap.put("token", token);
+        objectObjectHashMap.put("user", intelAccount1);
+        return objectObjectHashMap;
+    }
+
+    @Override
+    public Map<Object, Object> refreshPassword(IntelAccount intelAccount, String newPassword, String code) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        //校验当前验证码
+        String verifyCode = stringRedisTemplate.opsForValue().get(intelAccount.getPhoneNo());
+        if (!verifyCode.equals(code) ) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "验证码错误...");
+            return objectObjectHashMap;
+        }
+        //更新当前用户密码
+        int updateByPrimaryKeySelective = intelAccountMapper.updateByPrimaryKeySelective(intelAccount);
+        if (updateByPrimaryKeySelective == 0) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "更新用户失败...");
+            return objectObjectHashMap;
+        }
+        objectObjectHashMap.put("code", BaseConstant.appUserSuccessStatus);
+        objectObjectHashMap.put("msg", "更新操作成功...");
+        return objectObjectHashMap;
+    }
+
+    @Override
+    public Map<Object, Object> updatePassword(IntelAccount intelAccount, String newPassword, String oldpassword) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        //查询当前用户
+        IntelAccount intelAccount1 = intelAccountMapper.selectOne(intelAccount);
+        if (!intelAccount1.getPassword().equals(oldpassword) ) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "用户名或密码错误...");
+            return objectObjectHashMap;
+        }
+        //更新当前用户
+        int updateByPrimaryKeySelective = intelAccountMapper.updateByPrimaryKeySelective(intelAccount);
+        if (updateByPrimaryKeySelective == 0) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "更新用户失败...");
+            return objectObjectHashMap;
+        }
+        objectObjectHashMap.put("code", BaseConstant.appUserSuccessStatus);
+        objectObjectHashMap.put("msg", "更新操作成功...");
+        return objectObjectHashMap;
+    }
+
+    @Override
+    public Map<Object, Object> updatePhoneNum(IntelAccount intelAccount, String token, String newPhone, String code) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        //校验token
+        String tokenSession = stringRedisTemplate.opsForValue().get(intelAccount.getId());
+        if (!tokenSession.equals(token)) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "登陆信息有误,请重新登录...");
+            return objectObjectHashMap;
+        }
+        //检验验证码
+        String verifyCode = stringRedisTemplate.opsForValue().get(intelAccount.getPhoneNo());
+        if (!verifyCode.equals(code)) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "验证码有误...");
+            return objectObjectHashMap;
+        }
+        //查询当前用户
+        IntelAccount intelAccount1 = intelAccountMapper.selectOne(intelAccount);
+        if (!intelAccount1.getPhoneNo().equals(newPhone)) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "新号码不能与原号码一致...");
+            return objectObjectHashMap;
+        }
+        //更新手机号
+        int updateByPrimaryKeySelective = intelAccountMapper.updateByPrimaryKeySelective(intelAccount);
+        if (updateByPrimaryKeySelective  == 0) {
+            objectObjectHashMap.put("code", BaseConstant.appUserFaileStatus);
+            objectObjectHashMap.put("msg", "更新号码失败...");
+            return objectObjectHashMap;
+        }
+        objectObjectHashMap.put("code", BaseConstant.appUserSuccessStatus);
+        objectObjectHashMap.put("msg", "更新号码成功...");
         return objectObjectHashMap;
     }
 }
---
+++ src/test/java/com/company/restapi/service/impl/FounctionTest.java
@@ -0,0 +1,19 @@
+package com.company.restapi.service.impl;
+
+import org.junit.Test;
+
+/**
+ * com.company.restapi.service.impl
+ *
+ * @author Wang
+ * @create 2018-01-23 下午7:48
+ * @description:
+ **/
+public class FounctionTest {
+    @Test
+    public  void testMyidea(){
+
+        Integer i = 88888;
+        System.out.println(i.toString());
+    }
+}
